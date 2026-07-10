ce0d24b12df5daf312838ad7cb689f6a5c103bf0
JeoninRiverWater <128760945+JeoninRiverWater@users.noreply.github.com>
2026-07-10 10:47:44 +0900

    feat: initial commit of sudoku-react project with basic setup and assets

---
+++ sudoku-react/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>첫 React 프로젝트입니다.</p>
+    </>
+  );
+}
+
+export default App
---
+++ sudoku-react/src/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1>스도쿠</h1>;
+}
+
+export default Header;
