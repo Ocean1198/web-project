dd2a7789f715970130ae0c8b0a6b49a1eec8e5e0
JeoninRiverWater <128760945+JeoninRiverWater@users.noreply.github.com>
2026-08-31 15:27:46 +0900

    refactor: remove disabled prop from GameControls

--- sudoku-react/src/components/GameControls.tsx
+++ sudoku-react/src/components/GameControls.tsx
@@ -1,20 +1,17 @@
 type GameControlsProps = {
-  disabled: boolean;
   onCheck: () => void;
   onGiveUp: () => void;
 };
 
-function GameControls({ disabled, onCheck, onGiveUp } : GameControlsProps) {
+function GameControls({ onCheck, onGiveUp } : GameControlsProps) {
   return (
     <div>
       <button
-        disabled={disabled}
         onClick={onCheck}
       >
         Check
       </button>
       <button
-        disabled={disabled}
         onClick={onGiveUp}
       >
         Give Up
